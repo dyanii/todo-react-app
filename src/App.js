e9c911c88243049a8acb63a9679ada7ed06e1b08
= <=>
2023-04-12 19:35:55 +0900

    4/12 수업 종료

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React from "react";
+import Todo from "./Todo";
+import AddTodo from "./AddTodo.js";
+import { Paper, List, Container } from "@material-ui/core";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      items : [
+        {id:"0", title: "Hello World 1", done: true},
+        {id:"1", title: "Hello World 2", done: false},
+        {id:"2", title: "Hello World 3", done: true}
+      ]
+    };
+  }
+
+  add = (item) => {
+    const thisItems = this.state.items;
+    item.id = "ID-" + thisItems.length;
+    item.done= false;
+    thisItems.push(item);
+    this.setState({ items: thisItems });
+    console.log("items : ", this.state.items);
+  }
+
+
+  render() {
+    var todoItems = this.state.items.length > 0 && (
+      <Paper style={{ margin:16 }}>
+        <List>
+          {this.state.items.map((item, idx) => (
+          <Todo item={item} key={item.id} />
+          ))}
+        </List>
+      </Paper>
+    );
+
+
+    return (
+      <div className="App">
+        <Container maxWidth="md">
+          <AddTodo add={this.add} />
+          <div className="TodoList">{todoItems}</div>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default App;
